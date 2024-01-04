7e0c85093f8bf3979fae16bd8581d4e78840edb6
SuperSection <soumosarkar297@gmail.com>
2024-01-04 15:13:17 +0530

    add Resend for sending emails

--- actions/login.ts
+++ actions/login.ts
@@ -7,6 +7,7 @@ import { signIn } from "@/auth";
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { sendVerificationEmail } from "@/lib/mail";
 import { generateVerificationToken } from "@/lib/tokens";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
@@ -24,15 +25,18 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
     return { error: "Email does not exist!" };
   }
 
-/*
   if (!existingUser.emailVerified) {
     const verificationToken = await generateVerificationToken(
       existingUser.email
     );
 
+    await sendVerificationEmail(
+      verificationToken.email,
+      verificationToken.token
+    );
+
     return { success: "Confirmation email sent!" };
   }
-*/
 
   try {
     await signIn("credentials", {
